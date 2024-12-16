ecf78d04dd49ff623a6ed90ce5accdb49772369e
eczmeryem <eczmeryemyilmaz@gmail.com>
2024-12-16 12:51:13 +0300

    Entities added

---
+++ NTierArchitecture.Entities/Abstractions/Entity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitecture.Entities.Abstractions
+{
+    //abstract: Entity sınıfı new lenemez. Bu sınıftan yeni bir Entity nesnesi türetilsin istemiyoruz.
+    public abstract class Entity
+    {
+        public Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        public Guid Id { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }= DateTime.Now;
+    }
+}
---
+++ NTierArchitecture.Entities/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using NTierArchitecture.Entities.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTierArchitecture.Entities.Models
+{
+    //ICollection : Koleksiyon türleri için temel bir arayüzdür. List, HashSet,LinkedList, Dictionary...
+    public class Category : Entity
+    {
+        public string? CategoryName { get; set; }
+        public string? Description { get; set; }
+
+        //Navigation Property
+        public ICollection<Product>? Products { get; set; }
+
+    }
+}
